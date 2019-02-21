1044cd48a921050dabdc26f355cbce661afee988
Richard Batista <36830488+blue-stalfos@users.noreply.github.com>
2019-02-21 18:36:59 -0500

    Finalized audio player code

---  VIewController.swift
+++  VIewController.swift
@@ -1,32 +1,23 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 27/01/2016.
-//  Copyright © 2016 London App Brewery. All rights reserved.
-//
-
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 	
-	var player: AVAudioPlayer?
+	var audioPlayer: AVAudioPlayer!
 	
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
-		let url = Bundle.main.url(forResource: "note1", withExtension: "wav")!
+		let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
 		
 		do {
-			player = try AVAudioPlayer(contentsOf: url)
-			guard let player = player else { return }
-			
-			player.play()
-		} catch let error as NSError {
+			audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+		} catch {
 			print(error)
 		}
+		
+		audioPlayer.play()
     }
 }
