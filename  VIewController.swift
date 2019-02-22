d6dbd7f3f53cc991b79bf9830d5ecd5891498ff7
Richard Batista <36830488+blue-stalfos@users.noreply.github.com>
2019-02-21 19:28:30 -0500

    Finalized audio player code

---  VIewController.swift
+++  VIewController.swift
@@ -4,13 +4,21 @@ import AVFoundation
 class ViewController: UIViewController, AVAudioPlayerDelegate {
 	
 	var audioPlayer: AVAudioPlayer!
+	var selectedSoundFileName: String = ""
+	let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 	
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
-		let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+		selectedSoundFileName = soundArray[sender.tag - 1]
+		
+		playSound()
+    }
+	
+	func playSound() {
+		let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
 		
 		do {
 			audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
@@ -19,5 +27,5 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
 		}
 		
 		audioPlayer.play()
-    }
+	}
 }
