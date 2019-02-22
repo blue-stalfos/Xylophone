51e51566cd4be580a464c5344fd6807c0c8260a2
Richard Batista <36830488+blue-stalfos@users.noreply.github.com>
2019-02-22 14:27:19 -0500

    Corrected minor indentation and formatting

---  VIewController.swift
+++  VIewController.swift
@@ -6,13 +6,13 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
 	var audioPlayer: AVAudioPlayer!
 	let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 	
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
 
-    @IBAction func notePressed(_ sender: UIButton) {
+	@IBAction func notePressed(_ sender: UIButton) {
 		playSound(soundFileName: soundArray[sender.tag - 1])
-    }
+	}
 	
 	func playSound(soundFileName: String) {
 		let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
